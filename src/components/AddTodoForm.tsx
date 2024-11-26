f64b0711d40b150511677a500a399b92af3d8478
Papol14 <johnpaulescalona2@gmail.com>
2024-11-26 21:28:57 +0800

    Perform handleSubmit

--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+
 export default function AddTodoForm() {
+  const [input, setInput] = useState("");
+
+  function handleSubmit() {}
+
   return (
     <form className="flex">
       <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
         type="text"
         placeholder="What needs to be done?"
         className="rounded-s-md grow border border-gray-400 p-2"
