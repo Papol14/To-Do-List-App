0b3da624a9e934f2c2b0b0840dfd19215732f990
Papol14 <johnpaulescalona2@gmail.com>
2024-11-29 22:23:49 +0800

    Map some data

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,13 @@
 import { dummyData } from "./data/todos";
-import { useState } from "react";
-
-//Components
-import TodoItem from "./components/TodoItem";
-import AddTodoForm from "./components/AddTodoForm";
 
 const App = () => {
-  const [todos, setTodos] = useState(dummyData);
-
-  function setTodoCompleted(id: number, completed: boolean) {
-    setTodos((prevTodos) =>
-      prevTodos.map((todo) => (todo.id == id ? { ...todo, completed } : todo))
-    );
-  }
-
-  function addTodo(title: string) {
-    setTodos((prevTodos) => [
-      {
-        id: prevTodos.length + 1,
-        title,
-        completed: false,
-      },
-      ...prevTodos,
-    ]);
-  }
-
   return (
-    <main className="py-10 h-screen space-y-5">
-      <h1 className="text-3xl font-bold text-center">My App</h1>
-      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
-        <AddTodoForm onSubmit={addTodo} />
-        <div className="space-y-2">
-          {todos.map((todo) => (
-            <p key={todo.id} className="text-lg">
-              <TodoItem
-                todo={todo}
-                key={todo.id}
-                onCompletedChange={setTodoCompleted}
-              />
-            </p>
+    <main className="py-10 h-screen">
+      <h1 className="text-3xl font-bold text-center">Todo List</h1>
+      <div>
+        <div>
+          {dummyData.map((todo) => (
+            <p key={todo.id}>{todo.title}</p>
           ))}
         </div>
       </div>
