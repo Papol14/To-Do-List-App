18162880d7c736244520ada062335386da5d4274
Papol14 <johnpaulescalona2@gmail.com>
2024-11-30 16:51:43 +0800

    Declared setTodoCommpleted

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,23 @@ import { dummyData } from "./data/todos";
 import TodoItem from "./components/TodoItem";
 
 const App = () => {
+  function setTodoCompleted(id: number, completed: boolean) {
+    alert(
+      `The todo id of ${id} is now ${completed ? "completed" : "not completed"}`
+    );
+  }
+
   return (
     <main className="py-10 h-screen space-y-5">
       <h1 className="text-3xl font-bold text-center">Todo List</h1>
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
         <div className="space-y-2">
           {dummyData.map((todo) => (
-            <TodoItem key={todo.id} todo={todo} />
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              onCompletedChange={setTodoCompleted}
+            />
           ))}
         </div>
       </div>
