597acdaded47a716209c0072f94728298060f20b
Papol14 <johnpaulescalona2@gmail.com>
2024-11-30 16:43:39 +0800

    Set changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import { dummyData } from "./data/todos";
+import TodoItem from "./components/TodoItem";
 
 const App = () => {
   return (
-    <main className="py-10 h-screen">
+    <main className="py-10 h-screen space-y-5">
       <h1 className="text-3xl font-bold text-center">Todo List</h1>
-      <div className="max-w-lg mx-auto">
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
         <div className="space-y-2">
           {dummyData.map((todo) => (
-            <p key={todo.id} className="text-lg">
-              {todo.title}
-            </p>
+            <TodoItem key={todo.id} todo={todo} />
           ))}
         </div>
       </div>
